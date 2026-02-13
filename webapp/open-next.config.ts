c26f64a80b71d71e81970cebe90dc7df6ac1349f
seaprotocol <seaprotocol@seadex.org>
2026-02-13 12:16:45 +0800

    1. opennextjs 2. dockerfile

---
+++ webapp/open-next.config.ts
@@ -0,0 +1,5 @@
+// OpenNext configuration for Cloudflare Workers deployment
+// https://opennext.js.org/cloudflare
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({});
