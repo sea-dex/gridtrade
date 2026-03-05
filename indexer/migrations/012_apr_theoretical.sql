d27c91fec5a18c38a31c3b429a284f24d87161f4
seaprotocol <seaprotocol@seadex.org>
2026-03-05 15:26:48 +0800

    remove migrate files

--- indexer/migrations/012_apr_theoretical.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Rename apr_exclude_il to apr_theoretical
--- This aligns the field name with the updated apr.md specification
--- apr_theoretical represents the theoretical return if no grid trades occurred
-
-ALTER TABLE grids RENAME COLUMN apr_exclude_il TO apr_theoretical;
